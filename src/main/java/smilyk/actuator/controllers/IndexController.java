34ed3ee637714f8d41884488d66dfb709d635415
natalya khrebtiivsky <smilyk1982@gmail.com>
2020-06-22 17:24:21 +0300

    init project

--- src/main/java/smilyk/actuator/controllers/IndexController.java
+++ src/main/java/smilyk/actuator/controllers/IndexController.java
@@ -1,10 +1,10 @@
 package smilyk.actuator.controllers;
 
-import guru.springframework.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import smilyk.actuator.services.ProductService;
 
 
 @Controller
--- src/main/java/smilyk/actuator/controllers/ProductController.java
+++ src/main/java/smilyk/actuator/controllers/ProductController.java
@@ -1,15 +1,13 @@
 package smilyk.actuator.controllers;
 
-import guru.springframework.services.ProductService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import smilyk.actuator.services.ProductService;
 
-/**
- * Created by jt on 1/20/16.
- */
 @Controller
 public class ProductController {
 
