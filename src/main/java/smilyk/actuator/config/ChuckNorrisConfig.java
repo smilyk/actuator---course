406e58c1a4695631270b0099914b1378a029d252
natalya khrebtiivsky <smilyk1982@gmail.com>
2020-06-23 09:33:29 +0300

    adding ChuckNorris

---
+++ src/main/java/smilyk/actuator/config/ChuckNorrisConfig.java
@@ -0,0 +1,13 @@
+package smilyk.actuator.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisInfoContributor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckNorrisConfig {
+    @Bean
+    public ChuckNorrisInfoContributor chuckNorrisInfoContributor(){
+        return new ChuckNorrisInfoContributor();
+    }
+}
